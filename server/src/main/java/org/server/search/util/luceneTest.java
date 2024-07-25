6169ea17e6902c739f01315f3d1ae91e4ba11aac
gaoyvfeng <yufeng.gao@seres.cn>
2024-07-25 15:13:21 +0800

    search bool查询测试修改

--- server/src/main/java/org/server/search/util/luceneTest.java
+++ server/src/main/java/org/server/search/util/luceneTest.java
@@ -5,15 +5,10 @@ import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.index.StoredFields;
+import org.apache.lucene.index.*;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.*;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.IOUtils;
@@ -42,8 +37,16 @@ public class luceneTest {
         DirectoryReader ireader = DirectoryReader.open(directory);
         IndexSearcher isearcher = new IndexSearcher(ireader);
         // Parse a simple query that searches for "text":
-        QueryParser parser = new QueryParser("fieldname", analyzer);
-        Query query = parser.parse("text");
+//        QueryParser parser = new QueryParser("fieldname", analyzer);
+//        Query query = parser.parse("text");
+
+
+        BooleanQuery.Builder builder = new BooleanQuery.Builder();
+        builder.add(new TermQuery(new Term("fieldname", "text")), BooleanClause.Occur.MUST);
+        builder.add(new TermQuery(new Term("fieldname", "text")), BooleanClause.Occur.SHOULD);
+//        builder.add(new TermQuery(new Term("fieldname", "text")), BooleanClause.Occur.MUST_NOT);
+        BooleanQuery query = builder.build();
+
         ScoreDoc[] hits = isearcher.search(query, 10).scoreDocs;
         assertEquals(1, hits.length);
         // Iterate through the results:
