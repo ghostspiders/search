9efe9404291416634054c898d66a543677ab9d5d
gaoyvfeng <yufeng.gao@seres.cn>
2024-08-02 18:03:13 +0800

    search bool查询测试修改

--- server/src/main/java/org/server/search/search/dfs/DfsPhase.java
+++ server/src/main/java/org/server/search/search/dfs/DfsPhase.java
@@ -36,17 +36,28 @@ public class DfsPhase implements SearchPhase {
     }
 
     public void execute(SearchContext context) {
+        // 尝试执行以下代码块，如果发生异常，则捕获并处理
         try {
+            // 重写查询，可能涉及查询的优化或转换
             context.rewriteQuery();
-
+            // 创建一个线程安全的集合来存储查询中提取的词项
             THashSet<Term> termsSet = new THashSet<Term>();
-//            context.query().extractTerms(termsSet);
+            // 从查询中提取所有词项，并将它们添加到termsSet中
+            context.query().extractTerms(termsSet);
+            // 将termsSet转换为Term数组
             Term[] terms = termsSet.toArray(new Term[termsSet.size()]);
-//            int[] freqs = context.searcher().docFreqs(terms);
-
-//            context.dfsResult().termsAndFreqs(terms, freqs);
+            // 使用SearchContext中的搜索器获取每个词项在整个索引中的文档频率
+            int[] freqs = new int[terms.length];
+            // 遍历词条数组，获取每个词条的文档频率
+            for (int i = 0; i < terms.length; i++) {
+                freqs[i] =  context.searcher().getIndexReader().docFreq(terms[i]);
+            }
+            // 将收集到的词项及其频率信息存储到分布式搜索结果中
+            context.dfsResult().termsAndFreqs(terms, freqs);
+            // 获取索引中的文档总数，并将其设置到分布式搜索结果中
             context.dfsResult().numDocs(context.searcher().getIndexReader().numDocs());
         } catch (Exception e) {
+            // 如果执行过程中发生异常，抛出DfsPhaseExecutionException异常
             throw new DfsPhaseExecutionException(context);
         }
     }
